3b4b4ecf7883278238769322ad9b6c945e350370
MarcondesJuniorDev <mncjdev@gmail.com>
2024-04-16 17:11:57 -0400

    Criação de componentes de fazem parte da Implementação do site

---
+++ app/Livewire/Site/Partials/Footer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Site\Partials;
+
+use Livewire\Component;
+
+class Footer extends Component
+{
+    public function render()
+    {
+        return view('livewire.site.partials.footer');
+    }
+}
