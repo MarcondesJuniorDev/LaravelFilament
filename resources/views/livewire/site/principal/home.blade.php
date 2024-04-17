4372eac2b19e717c799dbbfd621d861df6aa98cc
MarcondesJuniorDev <mncjdev@gmail.com>
2024-04-17 10:59:25 -0400

    Arquivos para outras pastas para melhorar a hierarquia e organização das paginas e componentes da pagina principal do site

--- resources/views/livewire/site/principal/home.blade.php
+++ resources/views/livewire/site/principal/home.blade.php
@@ -3,7 +3,7 @@
         @livewire('site.partials.banners')
         @livewire('site.partials.awards')
         @livewire('site.partials.numbers')
-        @livewire('site.partials.awards')
-        @livewire('site.partials.awards')
+        @livewire('site.partials.news')
+        @livewire('site.partials.projects')
     </div>
 </div>
